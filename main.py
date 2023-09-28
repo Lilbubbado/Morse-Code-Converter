50d17e2a7a6e37ee0ca573abca7d2636edbde933
AdamLocke <17187387-AdamLocke@users.noreply.replit.com>
2023-09-28 11:10:26 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,72 @@
+CHARS_TO_MORSE_CODE_MAPPING = {
+    'A': '.-',
+    'B': '-...',
+    'C': '-.-.',
+    'D': '-..',
+    'E': '.',
+    'F': '..-.',
+    'G': '--.',
+    'H': '....',
+    'I': '..',
+    'J': '.---',
+    'K': '-.-',
+    'L': '.-..',
+    'M': '--',
+    'N': '-.',
+    'O': '---',
+    'P': '.--.',
+    'Q': '--.-',
+    'R': '.-.',
+    'S': '...',
+    'T': '-',
+    'U': '..-',
+    'V': '...-',
+    'W': '.--',
+    'X': '-..-',
+    'Y': '-.--',
+    'Z': '--..',
+    '1': '.----',
+    '2': '..---',
+    '3': '...--',
+    '4': '....-',
+    '5': '.....',
+    '6': '-....',
+    '7': '--...',
+    '8': '---..',
+    '9': '----.',
+    '0': '-----',
+    '.': '.-.-.-',
+    ',': '--..--',
+    '?': '..--..',
+    '\'': '· − − − − ·',
+    '!': '− · − · − −',
+    '/': '− · · − ·',
+    '(': '− · − − ·',
+    ')': '− · − − · −',
+    '&': '· − · · ·',
+    ':': '− − − · · ·',
+    ';': '− · − · − ·',
+    '=': '− · · · −',
+    '+': '· − · − ·',
+    '-': '− · · · · −',
+    '_': '· · − − · −',
+    '"': '· − · · − ·',
+    '$': '· · · − · · −',
+    '@': '· − − · − ·',
+    ' ': ' | ',
+}
+
+
+user_input = input("Enter the text you would like to convert to Morse Code: ").upper()
+
+output = ""
+
+for char in user_input:
+    if char not in CHARS_TO_MORSE_CODE_MAPPING and char != ' ':
+      print('Please try again with valid characters.')
+    else:
+      for mapping in CHARS_TO_MORSE_CODE_MAPPING:
+        if char == mapping:
+          output += f'{CHARS_TO_MORSE_CODE_MAPPING[mapping]} '
+
+print(f'Your Morse Code is: {output}')
